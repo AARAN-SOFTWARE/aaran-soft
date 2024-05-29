8538f3bd4c8b8a1a93195072a7285e019498320d
“SUNDAR-Office” <aaranoffice@gmail.com>
2024-05-29 19:00:06 +0530

    #366 refactor

--- resources/views/livewire/developer/ui-task/index.blade.php
+++ resources/views/livewire/developer/ui-task/index.blade.php
@@ -120,8 +120,8 @@
                     <x-input.model-text wire:model="vname" :label="'Title'"/>
 
                     <div class="px-1 py-4">
-{{--                        <x-input.rich-text wire:model="body" :placeholder="'Assign task here'"/>--}}
-                        <x-editor.simpleMDE wire:model="body"/>
+                        <x-input.rich-text wire:model="body" :placeholder="'Assign task here'"/>
+{{--                        <x-editor.simpleMDE wire:model="body"/>--}}
 
 
                     </div>
