fe51c0cc229aebbd29a240160daac13cbe70c792
kalai <kalaiararasan200229@gmail.com>
2024-06-03 13:42:44 +0530

    #374 refactor invoice

--- resources/views/pdf/offset/sales/sara_screens.blade.php
+++ resources/views/pdf/offset/sales/sara_screens.blade.php
@@ -455,8 +455,7 @@
                     <img src="{{ public_path('images/sara_screen.png')}}" alt="logo" width=""/>
                 </span>
             <span class="comp-address">
-                    <p>{{$cmp->get('address_1')}}</p>
-                    <p>{{$cmp->get('address_2')}},{{$cmp->get('city')}}</p>
+                    <p>{{$cmp->get('address_1')}}, {{$cmp->get('address_2')}},{{$cmp->get('city')}}</p>
                     <p>{{$cmp->get('contact')}} - {{$cmp->get('email')}}</p>
                     <p>{{$cmp->get('gstin')}}&nbsp;-&nbsp;{{$cmp->get('msme')}}</p>
                 </span>
@@ -713,8 +712,7 @@
                     <img src="{{ public_path('images/sara_screen.png')}}" alt="logo" width=""/>
                 </span>
             <span class="comp-address">
-                    <p>{{$cmp->get('address_1')}}</p>
-                    <p>{{$cmp->get('address_2')}},{{$cmp->get('city')}}</p>
+                    <p>{{$cmp->get('address_1')}}, {{$cmp->get('address_2')}},{{$cmp->get('city')}}</p>
                     <p>{{$cmp->get('contact')}} - {{$cmp->get('email')}}</p>
                     <p>{{$cmp->get('gstin')}}&nbsp;-&nbsp;{{$cmp->get('msme')}}</p>
                 </span>
