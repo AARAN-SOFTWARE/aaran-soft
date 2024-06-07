a373b00ff2714a97a97990f2f32af3be997eafc8
SUNDAR <aaransoftware@gmail.com>
2024-06-07 08:38:30 +0530

    #386 rework on share trades

--- aaran/Sundar/routes.php
+++ aaran/Sundar/routes.php
@@ -16,6 +16,7 @@ Route::middleware(['auth:sanctum', 'verified'])->group(function () {
     Route::get('shareTrades/deposits', App\Livewire\Sundar\Trading\Deposit::class)->name('shareTrades.deposits');
     Route::get('shareTrades/profits', App\Livewire\Sundar\Trading\Profit::class)->name('shareTrades.profits');
     Route::get('shareTrades/charges', App\Livewire\Sundar\Trading\Charges::class)->name('shareTrades.charges');
+    Route::get('shareTrades/summary', App\Livewire\Sundar\Trading\ShareTradeList::class)->name('shareTrades.summary');
 
 
 });
