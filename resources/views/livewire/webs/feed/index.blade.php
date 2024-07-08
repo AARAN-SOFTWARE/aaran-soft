b0c6fa77ddeb41dbe8b1328a524ba6f5b5f14c56
azhagiri <azhagirishankar5@gmail.com>
2024-07-08 13:15:46 +0530

    #486 feeds page

--- resources/views/livewire/webs/feed/index.blade.php
+++ resources/views/livewire/webs/feed/index.blade.php
@@ -17,7 +17,8 @@
                         <x-dashboard.welfare.search />
                     </div>
                     <div class="w-44 flex justify-between items-center">
-                        <div><x-icons.icon icon="notification"  class="w-6 h-6 mt-2 hover:text-red-700"/></div>
+                        <x-dashboard.welfare.notification />
+
                         <div>
                            <x-dashboard.welfare.create-new-red />
                         </div>
@@ -31,14 +32,39 @@
                 <!-- Feed -->
                 <div class="w-[98%] mx-auto text-xl mt-10 font-roboto font-semibold">Feed</div>
                 <div class="grid grid-cols-3 mt-4 justify-items-center gap-6 font-roboto">
-                    @for($i=1; $i<=9; $i++)
-                        <a href="/feed/show">
-                            <x-dashboard.welfare.feed-index />
-                        </a>
-                    @endfor
+{{--                        <a href="/feed/show">--}}
+                    <x-dashboard.welfare.feed-index :list="$list" />
+{{--                        </a>--}}
                 </div>
             </div>
-
         </div>
     </div>
+
+
+    <!-- Create/ Edit Popup --------------------------------------------------------------------------------------->
+    <x-forms.create :id="$vid">
+        <x-input.model-text wire:model="vname" :label="'Caption'"/>
+        @error('vname')
+        <span class="text-red-500">{{  $message }}</span>
+        @enderror
+        <x-input.model-text wire:model="description" :label="'Description'"/>
+        <x-input.model-text wire:model="bookmark" :label="'BookMark'"/>
+        <div class="relative mt-5 w-44 h-16 ml-[30rem]">
+            <div class="text-center mx-auto items-center">
+                <label>
+                    <input type="file" wire:model="image" class="hidden">
+                    <img src="../../../../images/upload-file.svg" alt="" class="h-5 w-5 text-gray-500">
+                </label>
+            </div>
+            <div class="absolute bottom-5 left-6">
+                <div class="mx-auto items-center mt-3 mr-1 ">
+                    @if($image)
+                        <img src="{{$isUploaded? $image->temporaryUrl() : url(\Illuminate\Support\Facades\Storage::url($image)) }}" alt="Image" class="h-16 w-28 mb-1 rounded-md outline outline-2 outline-gray-300 shadow-lg shadow-gray-400">
+                    @else
+                        <x-icons.icon :icon="'logo'" class=""/>
+                    @endif
+                </div>
+            </div>
+        </div>
+    </x-forms.create>
 </div>
