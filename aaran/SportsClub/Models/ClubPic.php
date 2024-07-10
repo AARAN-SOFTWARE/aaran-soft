3fda771a1cb7a294b5b2a814cf24db78c457f5b1
Divya <divyadharshini146@gmail.com>
2024-07-10 10:09:26 +0530

    #500 working on sports master....

---
+++ aaran/SportsClub/Models/ClubPic.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Aaran\SportsClub\Models;
+
+use Aaran\Common\Models\City;
+use Aaran\Common\Models\Pincode;
+use Aaran\Common\Models\State;
+use App\Models\User;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class ClubPic extends Model
+{
+
+    protected $guarded = [];
+
+    public static function search(string $searches)
+    {
+        return empty($searches) ? static::query()
+            : static::where('vname', 'like', '%' . $searches . '%');
+    }
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+
+
+}
