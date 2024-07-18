b98e2a2f03b21c6a8540ead358ee857a75c1e7fb
SUNDAR <aaransoftware@gmail.com>
2024-07-18 21:21:27 +0530

    #519 re-working for second database zync

--- app/Livewire/Zync/Audit/SalesTrack/Index.php
+++ app/Livewire/Zync/Audit/SalesTrack/Index.php
@@ -2,13 +2,86 @@
 
 namespace App\Livewire\Zync\Audit\SalesTrack;
 
+use Aaran\Audit\Models\Client;
+use Aaran\Audit\Models\SalesTrack\Rootline;
+use Aaran\Audit\Models\SalesTrack\RootlineItems;
 use App\Livewire\Trait\CommonTrait;
-use Illuminate\Support\Str;
+use Illuminate\Support\Facades\DB;
 use Livewire\Component;
 
 class Index extends Component
 {
     use CommonTrait;
+
+    #region[clients]
+    public function clients(): void
+    {
+        DB::table('clients')->delete();
+
+        DB::statement("ALTER TABLE clients AUTO_INCREMENT = 1;");
+
+        $rootLines = DB::connection('mariadb_web')->table('clients')->get();
+
+        foreach ($rootLines as $row) {
+            Client::create([
+                'id' => $row->id,
+                'vname' => $row->vname,
+                'group' => $row->group,
+                'payable' => $row->payable,
+                'active_id' => $row->active_id,
+                'user_id' => '1',
+            ]);
+        }
+        $this->dispatch('notify', ...['type' => 'success', 'content' => 'Clients Sync Successfully']);
+    }
+    #endregion
+
+
+    #region[rootline]
+    public function rootLine(): void
+    {
+        DB::table('rootlines')->delete();
+
+        DB::statement("ALTER TABLE rootlines AUTO_INCREMENT = 1;");
+
+        $rootLines = DB::connection('mariadb_web')->table('rootlines')->get();
+
+        foreach ($rootLines as $rootLine) {
+            Rootline::create([
+                'id' => $rootLine->id,
+                'vname' => $rootLine->vname,
+                'active_id' => $rootLine->active_id,
+                'user_id' => '1',
+            ]);
+        }
+        $this->dispatch('notify', ...['type' => 'success', 'content' => 'Rootline Sync  Successfully']);
+    }
+    #endregion
+
+
+    #region[rootline items]
+    public function rootLineItems(): void
+    {
+        DB::table('rootline_items')->delete();
+
+        DB::statement("ALTER TABLE rootline_items AUTO_INCREMENT = 1;");
+
+
+        $rootLines = DB::connection('mariadb_web')->table('rootline_items')->get();
+
+        foreach ($rootLines as $row) {
+
+            RootlineItems::create([
+                'serial' => $row->serial,
+                'rootline_id' => $row->rootline_id,
+                'client_id' => $row->client_id,
+                'active_id' => $row->active_id ?: '0',
+                'user_id' => auth()->id(),
+            ]);
+        }
+        $this->dispatch('notify', ...['type' => 'success', 'content' => 'Rootline items Sync Successfully']);
+    }
+    #endregion
 
     #region[render]
     public function render()
--- resources/views/livewire/zync/audit/sales-track/index.blade.php
+++ resources/views/livewire/zync/audit/sales-track/index.blade.php
@@ -3,5 +3,33 @@
 
     <x-forms.m-panel>
 
+        <div class="flex gap-3">
+
+            <button wire:click="clients" class="bg-lime-500 px-2 py-2 rounded-md">Clients</button>
+
+            <button wire:click="rootLine" class="bg-amber-500 px-2 py-2 rounded-md">Rootline</button>
+
+            <button wire:click="rootLineItems" class="bg-green-500 px-2 py-2 rounded-md text-white">
+                Rootline Items
+            </button>
+
+            <button wire:click="rootLineItems" class="bg-blue-500 px-2 py-2 rounded-md text-white">
+                Sales Track
+            </button>
+
+            <button wire:click="rootLineItems" class="bg-red-500 px-2 py-2 rounded-md text-white">
+                Sales Track Items
+            </button>
+
+            <button wire:click="rootLineItems" class="bg-violet-500 px-2 py-2 rounded-md text-white">
+                Sales Bill
+            </button>
+
+            <button wire:click="rootLineItems" class="bg-gray-500 px-2 py-2 rounded-md text-white">
+                Sales Bill Items
+            </button>
+
+        </div>
+
     </x-forms.m-panel>
 </div>
