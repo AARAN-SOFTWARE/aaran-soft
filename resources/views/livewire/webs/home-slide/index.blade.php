0fec57fbaf52fa65a91044d53d1b71c27dd6983f
SUNDAR <aaransoftware@gmail.com>
2024-08-03 11:12:28 +0530

    #549 refactor sports club

--- resources/views/livewire/webs/home-slide/index.blade.php
+++ resources/views/livewire/webs/home-slide/index.blade.php
@@ -57,14 +57,14 @@
                         <x-table.cell-text center>
                             <div class="flex-shrink-0 h-12 w-12 rounded-xl mx-auto mt-2">
                                 <img src="{{ \Illuminate\Support\Facades\Storage::url('/images/'.$row->bg_image)}}"
-                                     alt="bg_image"/>
+                                     alt="{{$row->bg_image}}"/>
                             </div>
                         </x-table.cell-text>
 
                         <x-table.cell-text center>
                             <div class="flex-shrink-0 h-12 w-12 rounded-xl mx-auto mt-2">
                                 <img src="{{ \Illuminate\Support\Facades\Storage::url('/images/'.$row->cont_image)}}"
-                                     alt="cont_image"/>
+                                     alt="{{$row->cont_image}}"/>
                             </div>
                         </x-table.cell-text>
 
