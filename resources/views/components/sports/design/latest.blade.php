2e2245831449dfe370caf79b9ee104030c6065a4
azhagiri <azhagirishankar5@gmail.com>
2024-08-08 11:20:49 +0530

    #563 working on sports club latest

--- resources/views/components/sports/design/latest.blade.php
+++ resources/views/components/sports/design/latest.blade.php
@@ -5,6 +5,9 @@
     "news"=>null,
     "moments"=>null
 ])
+<!-- #DEFCE8-->
+<!-- #EDFCF2-->
+<!-- #19398A-->
 
 <div class="h-24">&nbsp;</div>
 <div class="md:h-[42rem] h-auto flex-col flex gap-y-6 animate__animated wow animate__fadeInUpBig"
@@ -12,30 +15,30 @@
     <div class="w-11/12 mx-auto text-white font-Don text-2xl tracking-wider mt-16">Latest</div>
     <div class="w-11/12 grid md:grid-cols-3 grid-cols-1 mx-auto gap-6 mt-4">
         <x-sports.slider.slider-1 :list="$upComingEvents"/>
-        <x-sports.slider.slider-2 :list="$blogs"/>
-        <x-sports.slider.slider-3 :list="$events"/>
+        <x-sports.slider.slider-1 :list="$blogs"/>
+        <x-sports.slider.slider-1 :list="$events"/>
     </div>
     <div class="w-11/12 grid md:grid-cols-2 grid-cols-1 gap-6 mx-auto">
-        <x-sports.slider.slider-4 :list="$news"/>
-        <x-sports.slider.slider-5 :list="$moments"/>
+        <x-sports.slider.slider-1 :list="$news"/>
+        <x-sports.slider.slider-1 :list="$moments"/>
     </div>
     <div class="w-11/12 grid md:grid-cols-3 grid-cols-1 mx-auto gap-6">
         <a href="/gallery">
             <div
-                class=" h-44 bg-[#19398A] rounded-lg flex-col flex justify-center items-center text-white font-roboto text-xl">
+                class=" h-44 bg-[#DEFCE8] rounded-lg flex-col flex justify-center items-center text-[#19398A] font-roboto text-xl">
                 <x-icons.icon icon="photo" class="w-16 h-16"/>
                 <div>Pics</div>
             </div>
         </a>
-        <div class="w-h-44 bg-gradient-to-br from-[#19398A] to-orange-500 rounded-lg
+        <div class="w-h-44 bg-gradient-to-br from-[#DEFCE8] to-[#EDFCF2] rounded-lg
         flex-col flex justify-center items-center text-white ">
-            <div class="font-bold font-gab text-xl tracking-wider bg-gradient-to-r from-white via-yellow-500 to-cyan-500 inline-block text-transparent bg-clip-text">Interesting Facts</div>
+            <div class="font-bold font-gab text-xl tracking-wider bg-gradient-to-r from-red-600 via-yellow-500 to-cyan-500 inline-block text-transparent bg-clip-text">Interesting Facts</div>
             <div
-                class="font-roboto tracking-wider text-sm p-5 text-center">{!! App\Helper\Slogan::getRandomQuote() !!}</div>
+                class="font-roboto tracking-wider text-[#19398A] text-sm p-5 text-center">{!! App\Helper\Slogan::getRandomQuote() !!}</div>
         </div>
         <a href="/news">
             <div
-                class=" h-44 bg-[#19398A] rounded-lg flex-col flex justify-center items-center text-white font-roboto text-xl">
+                class=" h-44 bg-[#DEFCE8] rounded-lg flex-col flex justify-center items-center text-[#19398A] font-roboto text-xl">
                 <x-icons.icon icon="newspaper-outline" class="w-16 h-16"/>
                 <div>News</div>
             </div>
